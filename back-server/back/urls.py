9235c538115aa9dd4faed83fe5e67d58c3a4335f
chadireoroonu <chadireoroonu@gmail.com>
2023-05-17 17:42:55 +0900

    SJ_BACK_article_comment_model

--- back-server/back/urls.py
+++ back-server/back/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
     path('accounts/', include('dj_rest_auth.urls')),
-    path('accounts/signup/', include('dj_rest_auth.registration.urls'))
+    path('accounts/signup/', include('dj_rest_auth.registration.urls')),
+    path('articles/', include('articles.urls')),
 ]
