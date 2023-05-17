d398672bc5e4cbbe9f1ee221214e15ef1787c966
chadireoroonu <chadireoroonu@gmail.com>
2023-05-17 21:58:07 +0900

    SJ_BACK_article_model

--- back-server/articles/models.py
+++ back-server/articles/models.py
@@ -8,7 +8,7 @@ class Article(models.Model):
     # 안넣으면 생성 못하게 막기 -> views.py 작업, 지금하면 오류 반환
     img = models.ImageField() 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    like_user = models.ManyToManyField(User, blank=True, many=True)
+    like_user = models.ManyToManyField(User, related_name='like_articles')
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
