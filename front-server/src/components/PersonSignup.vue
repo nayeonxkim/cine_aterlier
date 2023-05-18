8b3be3b58db0a4a9f227d39c115b5f1bcc135dc1
chadireoroonu <chadireoroonu@gmail.com>
2023-05-18 16:34:01 +0900

    SJ_FRONT_login

--- front-server/src/components/PersonSignup.vue
+++ front-server/src/components/PersonSignup.vue
@@ -31,6 +31,12 @@ export default {
       const username = this.username
       const password1 = this.password1
       const password2 = this.password2
+
+      const payload = {
+        username, password1, password2
+      }
+
+      this.$store.dispatch('signUp', payload)
     }
   }
 }
