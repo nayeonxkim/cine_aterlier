9e32648bb026210d1036e77d4d312a865f044920
chadireoroonu <chadireoroonu@gmail.com>
2023-05-18 11:20:38 +0900

    SJ_BACK_article_api

--- back-server/articles/models.py
+++ back-server/articles/models.py
@@ -2,14 +2,12 @@ from django.db import models
 from accounts.models import User
 
 class Article(models.Model):
-    img = models.ImageField(blank=False, upload_to="%Y/%m/%d") 
+    img = models.ImageField(blank=False) 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    like_user = models.ManyToManyField(User, related_name='like_articles')
+    like_user = models.ManyToManyField(User, blank=True, related_name='like_articles')
     title = models.CharField(max_length=100)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
 
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
