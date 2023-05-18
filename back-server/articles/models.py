b47339138a6da9dc439e248118684f263991e595
chadireoroonu <chadireoroonu@gmail.com>
2023-05-18 10:11:59 +0900

    SJ_BACK_article_api

--- back-server/articles/models.py
+++ back-server/articles/models.py
@@ -1,18 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# class User(AbstractUser):
-#     pass
+from accounts.models import User
 
 class Article(models.Model):
-    img = models.ImageField(blank=False) 
+    img = models.ImageField(blank=False, upload_to="%Y/%m/%d") 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     like_user = models.ManyToManyField(User, related_name='like_articles')
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
-# class Comment(models.Model):
-#     article = models.ForeignKey(Article, on_delete=models.CASCADE)
-#     content = models.TextField()
-#     created_at = models.DateTimeField(auto_now_add=True)
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- back-server/articles/serializers.py
+++ back-server/articles/serializers.py
@@ -2,11 +2,12 @@ from rest_framework import serializers
 from .models import Article, Comment
 
 class ArticleSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(use_url=True)
+
     class Meta:
         model = Article
         fields = '__all__'
         read_only_fields = ['img', 'author', 'like_user',]
-
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
