978f4678c223d7d394a14efc14e1cadc9bc4fee8
chadireoroonu <chadireoroonu@gmail.com>
2023-05-18 14:30:16 +0900

    SJ_FRONT_login

--- front-server/src/App.vue
+++ front-server/src/App.vue
@@ -4,19 +4,29 @@
       <router-link to="/home">Home</router-link> |
       <router-link to="/movie">Movie</router-link> |
       <router-link to="/community">Community</router-link> |
-      <router-link to="/login">Login</router-link>
+      <router-link to="/personal">Personal</router-link>
     </nav>
     <router-view/>
   </div>
 </template>
 
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
 
 nav {
