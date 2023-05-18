3b45a8caeccad41e1c6a0a04efae26c521da38b7
chadireoroonu <chadireoroonu@gmail.com>
2023-05-18 10:11:59 +0900

    SJ_BACK_article_api

--- back-server/articles/urls.py
+++ back-server/articles/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from . import views
+
+# app_name = 'articles'
 
 urlpatterns = [
-    
+    path('index/', views.index),
+    path('create/', views.create),
+    # path('<int:article_pk>/', views.detail),
+    # path('update/<int:article_pk>/', views.update)
 ]
--- back-server/articles/views.py
+++ back-server/articles/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, get_list_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+from rest_framework import status
+
+from .models import Article, Comment
+from .serializers import ArticleSerializer, CommentSerializer
+
+# 게시글 전체 조회
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def index(request):
+    article = Article.objects.all()
+    serializer = ArticleSerializer(article, many=True)
+    return Response(serializer.data)
+
+# 게시글 생성
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def create(request):
+    serializer = ArticleSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+        return Response(serializer.data)
+    
+# 게시글 상세 조회
+# @api_view(['GET'])
+# @permission_classes([AllowAny])
+# def detail(request, article_pk):
+#     article = get_object_or_404(Article, pk=article_pk)
+#     serializer = ArticleSerializer(article)
+#     return Response(serializer.data)
+
+# @api_view([''])
