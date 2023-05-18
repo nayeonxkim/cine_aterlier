1e14c3eebe7f39cc3fe57b2459c6778e1a74c52b
nayeon <kny0628m@gmail.com>
2023-05-18 16:01:53 +0900

    NY_FRONT_MovieView, BACK_loadData

--- front-server/src/components/MovieList.vue
+++ front-server/src/components/MovieList.vue
@@ -1,19 +1,39 @@
 <template>
   <div>
     <h1>Movie List</h1>
-    <MovieDetail/>
+    <MovieItem 
+    v-for="movie in movieList"
+    :key="movie.tmdb_id" 
+    :movie-item="movie"
+     />
+
   </div>
 </template>
 
 <script>
-import MovieDetail from '@/components/MovieDetail.vue'
+import MovieItem from './MovieItem.vue'
 
 export default {
   name: 'MovieList',
   components: {
-    MovieDetail,
-  }
+    MovieItem
+  },
   // store에서 모든 영화 정보 가져오기
+  methods:{
+    get_movie_list(){
+      this.$store.dispatch('get_movie_list')
+    },
+
+  },
+  computed:{
+    movieList(){
+      return this.$store.state.movieList
+    }
+  },
+  created(){
+    this.get_movie_list()
+  }
+
 }
 </script>
 
--- front-server/src/router/index.js
+++ front-server/src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import MovieDetail from '../components/MovieDetail.vue'
 import HomeView from '../views/HomeView.vue'
 import MovieView from '../views/MovieView.vue'
-import LoginView from '../views/LoginView.vue'
-import CommunityView from '../views/CommunityView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/home',
+    path: '/',
     name: 'home',
     component: HomeView
   },
@@ -17,25 +16,14 @@ const routes = [
     path: '/movie',
     name: 'movie',
     component: MovieView
+
   },
   {
-    path: '/login',
-    name: 'login',
-    component: LoginView
-  },
-  {
-    path: '/community',
-    name: 'community',
-    component: CommunityView
-  },
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/MovieView.vue')
-  // }
+    path: '//movie-detail/:movieId',
+    name: 'movie-detail',
+    component: MovieDetail
+
+  }
 ]
 
 const router = new VueRouter({
