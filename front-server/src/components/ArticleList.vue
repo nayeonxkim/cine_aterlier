1a6131cf6b3a1ee720cbffc78cb1cea25da0e60d
chadireoroonu <chadireoroonu@gmail.com>
2023-05-18 17:45:45 +0900

    SJ_FRONT_login

---
+++ front-server/src/components/ArticleList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Article List</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'ArticleList',
+  components: {
+    // ArticleItem,
+  },
+  computed: {
+    isLogin() {
+      return this.$store.getters.isLogin
+    }
+  },
+  created() {
+    this.getArticles()
+  },
+  methods: {
+    getArticles() {
+      if (this.isLogin) {
+        this.$store.dispatch('getArticles')
+      } else {
+        alert('로그인이 필요한 페이지입니다.')
+        this.$router.push({ name: 'personlogin' })
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
