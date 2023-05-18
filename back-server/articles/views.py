ed61754423cba51bbe8028e119277138e0c4430a
chadireoroonu <chadireoroonu@gmail.com>
2023-05-19 01:00:19 +0900

    SJ_FRONT_coummunity

--- back-server/articles/views.py
+++ back-server/articles/views.py
@@ -3,7 +3,9 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 from rest_framework.decorators import permission_classes
-from rest_framework.permissions import AllowAny
+
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 from rest_framework import status
 
@@ -12,7 +14,7 @@ from .serializers import ArticleSerializer, CommentSerializer
 
 # 게시글 전체 조회
 @api_view(['GET'])
-@permission_classes([AllowAny])
+@permission_classes([IsAuthenticated])
 def index(request):
     article = Article.objects.all()
     serializer = ArticleSerializer(article, many=True)
@@ -20,7 +22,7 @@ def index(request):
 
 # 게시글 생성
 @api_view(['POST'])
-@permission_classes([AllowAny])
+@permission_classes([IsAuthenticated])
 def create(request):
     serializer = ArticleSerializer(data=request.data)
     if serializer.is_valid(raise_exception=True):
@@ -29,7 +31,7 @@ def create(request):
     
 # 게시글 상세 조회
 @api_view(['GET'])
-@permission_classes([AllowAny])
+@permission_classes([IsAuthenticated])
 def detail(request, article_pk):
     article = get_object_or_404(Article, pk=article_pk)
     serializer = ArticleSerializer(article)
@@ -37,7 +39,7 @@ def detail(request, article_pk):
 
 # 게시글 수정
 @api_view(['PUT'])
-@permission_classes([AllowAny])
+@permission_classes([IsAuthenticated])
 def update(request, article_pk):
     article = get_object_or_404(Article, pk=article_pk)
     serializer = ArticleSerializer(article, data=request.data)
@@ -47,7 +49,7 @@ def update(request, article_pk):
 
 # 게시글 삭제
 @api_view(['DELETE'])
-@permission_classes([AllowAny])
+@permission_classes([IsAuthenticated])
 def delete(request, article_pk):
     article = get_object_or_404(Article, pk=article_pk)
     article.delete()
@@ -55,7 +57,7 @@ def delete(request, article_pk):
 
 # 댓글 생성
 @api_view(['POST'])
-@permission_classes([AllowAny])
+@permission_classes([IsAuthenticated])
 def comment_create(request, article_pk):
     article = get_object_or_404(Article, pk=article_pk)
     serializer = CommentSerializer(data=request.data)
@@ -65,7 +67,7 @@ def comment_create(request, article_pk):
     
 # 댓글 삭제
 @api_view(['DELETE'])
-@permission_classes([AllowAny])
+@permission_classes([IsAuthenticated])
 def comment_delete(request, article_pk, comment_pk):
     article = get_object_or_404(Article, pk=article_pk)
     comment = get_object_or_404(Comment, pk=comment_pk)
