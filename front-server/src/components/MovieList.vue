fc68966c33aa763ee7966bb48ce2056681c8929b
chadireoroonu <chadireoroonu@gmail.com>
2023-05-19 16:21:06 +0900

    SJ_FRONT_movie

---
+++ front-server/src/components/MovieList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>MovieList</h1>
+    <ul>
+      <li v-for="movie in movieList" :key="movie.id"></li>
+    </ul>
+    <MovieItem />
+  </div>
+</template>
+
+<script>
+import MovieItem from "@/components/MovieItem.vue";
+
+export default {
+  name: 'MovieList',
+  components: {
+    MovieItem
+  },
+  computed: {
+    movieList() {
+      return this.$store.state.movieList;
+    }
+  }
+}
+</script>
+
+<style>
+/* CSS styles for MovieList */
+</style>
