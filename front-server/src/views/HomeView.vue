5bda3f43763022aef1f4c4421381f10651288c25
nayeon <kny0628m@gmail.com>
2023-05-19 16:22:50 +0900

    NY_DESIGN_NAV frame

--- front-server/src/views/HomeView.vue
+++ front-server/src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="home">
-    <MyMovieImage/>,
-    <ChoiceList/>,
-    <MyMovieList/>
+    <div>
+      <MyMovieImage/>,
+      <ChoiceList/>,
+      <MyMovieList/>
+    </div>
   </div>
 </template>
 
@@ -41,3 +43,9 @@ export default {
   }
 }
 </script>
+
+<style>
+
+
+
+</style>
