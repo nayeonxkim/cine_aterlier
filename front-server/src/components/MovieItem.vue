9a9f8dcc9e30c57d4580cf539987de27ed834452
NayeonKim <kny0628m@gmail.com>
2023-05-19 22:01:15 +0900

    NY_BACK_RATING

--- front-server/src/components/MovieItem.vue
+++ front-server/src/components/MovieItem.vue
@@ -3,7 +3,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <div class="col">
       <div class="card">
-        <img :src="`https://image.tmdb.org/t/p/w500${movieItem.poster_path}`" class="card-img-top" alt="...">
+        <img @click="goToDetail" :src="`https://image.tmdb.org/t/p/w500${movieItem.poster_path}`" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">{{movieItem.title}}</h5>
         </div>
--- front-server/src/components/MyMovieList.vue
+++ front-server/src/components/MyMovieList.vue
@@ -5,8 +5,15 @@
 </template>
 
 <script>
+// import axios from 'axios'
 export default {
   // router view 중첩 사용
+  methods:{
+    get_movie_keyword(){
+      axios
+      .get(`http://127.0.0.1:8000/movies/${movie}/img/`)
+    }
+  }
 }
 </script>
 
