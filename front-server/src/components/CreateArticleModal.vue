2a49ae8df0cb5d188cc6daa685ae66a59d277041
chadireoroonu <chadireoroonu@gmail.com>
2023-05-21 01:25:20 +0900

    SJ_FRONT_community_modal

--- front-server/src/components/CreateArticleModal.vue
+++ front-server/src/components/CreateArticleModal.vue
@@ -1,23 +1,44 @@
 <template>
   <div>
-    <button @click="openModal">CREATE</button>
-    <div v-if="isModalOpen" class="modal">
-      <div class="modal-content">
-        <span class="close" @click="closeModal">&times;</span>
-        <h2>Create New Article</h2>
-        <form @submit.prevent="submitForm" enctype="multipart/form-data">
-          <input type="file" ref="fileInput" accept="image/*" required />
-          <input v-model="title" type="text" placeholder="Title" required />
-          <textarea v-model="content" placeholder="Content" required></textarea>
-          <button type="submit">Create</button>
-        </form>
+    <button @click="openModal" class="btn btn-secondary">CREATE</button>
+    <div class="modal" tabindex="-1" role="dialog" :class="{ 'show': isModalOpen }">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Create New Article</h5>
+            <button type="button" class="close" @click="closeModal">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <form @submit.prevent="submitForm" enctype="multipart/form-data">
+            <div class="modal-body">
+              <div class="form-group">
+                <label for="fileInput">Image:</label>
+                <input id="fileInput" type="file" ref="fileInput" accept="image/*" required>
+              </div>
+              <div class="form-group">
+                <label for="titleInput">Title:</label>
+                <input id="titleInput" v-model="title" type="text" class="form-control" placeholder="Title" required>
+              </div>
+              <div class="form-group">
+                <label for="contentInput">Content:</label>
+                <textarea id="contentInput" v-model="content" class="form-control" placeholder="Content" required></textarea>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="submit" class="btn btn-primary">Create</button>
+              <button type="button" class="btn btn-secondary" @click="closeModal">Close</button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// CreateArticleModal 컴포넌트
+import axios from 'axios'
+
 export default {
   name: 'CreateArticleModal',
   data() {
@@ -29,34 +50,43 @@ export default {
   },
   methods: {
     openModal() {
-      console.log('모달열림')
+      console.log('열림!')
       this.isModalOpen = true
+      console.log(this.isModalOpen)
     },
     closeModal() {
       this.isModalOpen = false
     },
     submitForm() {
+      const API_URL = 'http://127.0.0.1:8000'
       const formData = new FormData()
-      const file = this.$refs.fileInput.files[0]
-      formData.append('img', file)
+      formData.append('img', this.$refs.fileInput.files[0]) // 이미지 파일 추가
       formData.append('title', this.title)
       formData.append('content', this.content)
 
-      this.$store
-        .dispatch('create_article', formData)
-        .then(() => {
-          this.closeModal()
-          this.$emit('refresh-articles')
-        })
-        .catch(error => {
-          console.error(error)
-        })
+      // 유저 정보 가져오기
+      const user = this.$store.state.User
+      if (user) {
+        formData.append('author', user.id)
+      }
+
+      axios.post(`${API_URL}/articles/create/`, formData, {
+        headers: {
+          Authorization: `Token ${this.$store.state.token}`,
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then(() => {
+        console.log(this.Authorization)
+        this.$store.dispatch('get_article_list', 1)
+        this.closeModal()
+        this.title = ''
+        this.content = ''
+      })
+      .catch((error) => {
+        console.error(error)
+      })
     }
   }
 }
-
 </script>
-
-<style>
-
-</style>
