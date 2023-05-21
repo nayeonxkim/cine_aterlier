7080840ba36a45779ccad54d9699cd02c855bfd2
chadireoroonu <chadireoroonu@gmail.com>
2023-05-22 06:47:00 +0900

    SJ_FRONT_community article delete, comment loading

---
+++ front-server/src/components/ArticleComments.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Article Comments</h1>
+    <div class="mt-5">
+      <div v-for="comment in comments" :key="comment.id" class="card mt-3">
+        <div class="card-body">
+          <p>{{ comment.content }}</p>
+          <p class="text-muted">작성자: {{ comment.author }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+</style>
