5b2faa8366d65522c7098dd5afe433275c35689a
chadireoroonu <chadireoroonu@gmail.com>
2023-05-22 03:23:51 +0900

    SJ_FRONT_community pagenation load, methods etc, login

--- front-server/src/components/ArticleItem.vue
+++ front-server/src/components/ArticleItem.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <div class="card" @click="toDetail(articleItem)">
-      <img :src="getFullImagePath(articleItem.img)" alt="Article Image" class="card-img-top">
+    <div class="card" @click="navigate(`articles/${articleItem.id}`)">
+      <img
+      :src="getFullImagePath(articleItem.img)"
+      class="card-img-top"
+      style="width:100%; height:100%;"
+      alt="Article Image">
       <div class="card-body">
-        <h5 class="card-title">영화 제목</h5>
+        <h6 class="card-title">영화 제목</h6>
         <hr>
         <h6 class="card-subtitle mb-2 text-muted">화가 이름</h6>
       </div>
@@ -17,10 +21,12 @@ export default {
   props: {
     articleItem: Object
   },
+  computed: {
+    articleList() {
+      return this.$store.state.articleList
+    }
+  },
   methods: {
-    toDetail(article) {
-      this.$emit('to-detail', article)
-    },
     getFullImagePath(imageUrl) {
       const API_URL = 'http://127.0.0.1:8000'
       return `${API_URL}${imageUrl}`
