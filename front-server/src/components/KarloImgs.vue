f91c562c1289c78251fc2bf9472a81c3062a8ca8
nayeon <kny0628m@gmail.com>
2023-05-22 14:49:44 +0900

    NY_FRONT_Karlo multi Img before Carousel

--- front-server/src/components/KarloImgs.vue
+++ front-server/src/components/KarloImgs.vue
@@ -1,11 +1,7 @@
 <template>
   <div>
-    <v-carousel>
-    <v-carousel-item v-for="(item, index) in karloImgs" :key="index">
-        <img :src="`http://127.0.0.1:8000${item.img_url}`" alt="Carousel Image">
-      <!-- <img :src="`http://127.0.0.1:8000${karloImg.img_url}`" style="width: 200px; height: 200px;" alt=""> -->
-    </v-carousel-item>
-  </v-carousel>
+    <img :src="`http://127.0.0.1:8000${karloImg.img_url}`" style="width: 200px; height: 200px;" alt="">
+    <!-- <p>{{karloImg.img_url}}</p> -->
   </div>
 </template>
 
@@ -13,8 +9,16 @@
 export default {
   name:'KarloImgs',
   props:{
-    karloImgs:Object
+    karloImg:Object
   },
+  methods:{
+    KarloImgs_reset(){
+      return this.$store.commit('KARLOIMGS_RESET')
+    }
+  },
+  destroyed(){
+    this.KarloImgs_reset()
+  }
 }
 </script>
 
