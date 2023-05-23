f4dc34015d9c29a3658119aad8cc0ca20076c5c6
nayeon <kny0628m@gmail.com>
2023-05-23 17:05:59 +0900

    NY_FRONT_Movie Genre Filter Btn

--- front-server/src/components/MovieDetail.vue
+++ front-server/src/components/MovieDetail.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <div class="image-container">
+      <img :src="`https://image.tmdb.org/t/p/w500${movieDetail.backdrop_path}`" alt="" width="80%" height="80%">
+    </div>
+
       <h5 class="modal-title">{{movieDetail.title}}</h5>
       <img :src="`https://image.tmdb.org/t/p/w500${movieDetail.poster_path}`" alt="">
       <p>{{movieDetail.release_date}}</p>
@@ -8,9 +12,7 @@
       <p>{{movieDetail.runtime}}분</p>
       <p>{{movieDetail.popularity}}</p>
       <p>{{movieDetail.overview}}</p>
-       <div>
-        <iframe :src="`https://www.youtube.com/embed/${movieTrailer[0].id.videoId}`" frameborder="0"></iframe>
-      </div>
+      <p>{{movieDetail}}</p> 
   </div>
 </template>
 
@@ -44,7 +46,7 @@ export default {
       {params :{
         part : 'snippet',
         type : 'video',
-        key : 'AIzaSyCDAEYnm8SkPW3utCcWiAtpmGPaEjEWG0Q',
+        key : 'AIzaSyDqAHcO6_gAszogUufo0gNwDR7WGjEl_Mo',
         q : searchWord
       }})
       .then((response) =>{
@@ -53,16 +55,45 @@ export default {
     },
   },
 
-  created() {
-    this.get_movie_detail()
-    this.getTrailer()
-  },
+  // created() {
+  //   },
   mounted(){
-    this.getTrailer()
 
-  }
+    this.get_movie_detail()
+  },
+  // beforeUpdate(){
+  //   this.getTrailer()
+
+  // }
 }
 </script>
 
 <style>
+.image-container {
+  width: 100%;
+  height: 0;
+  padding-bottom: 56.25%; /* 이미지 비율에 따라 조정 */
+  position: relative;
+  overflow: hidden;
+}
+
+.image-container img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  filter: grayscale(70%);
+}
+
+.image-container::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.7));
+}
 </style>
--- front-server/src/views/MovieView.vue
+++ front-server/src/views/MovieView.vue
@@ -1,39 +1,35 @@
 <template>
   <div>
     <!-- <h1>TOP 100 Popular</h1> -->
+    <div class="genreBtn">
 
-    <MovieList />
-    <PageNav />
+    <router-link to="/movie/Top100">인기 Top100</router-link>
+    <button>코미디/드라마 (35, 18)</button>
+    <button>액션/어드벤쳐 (28, 12)</button>
+    <button>애니메이션 (16)</button>
+    <button>범죄/스릴러 (80, 53)</button>
+    <button>호러/미스터리 (27, 9648)</button>
+    <button>SF/판타지 (878, 14)</button>
+    <button>전쟁/역사 (10752, 36)</button>
+    <button>음악/다큐멘터리 (10402, 99)</button>
+    </div>
+
+    <router-view />
   </div>
 </template>
 
 <script>
-import MovieList from '../components/MovieList.vue'
-import PageNav from '@/components/PageNav.vue'
-
 export default {
   name: 'MovieView',
   components: {
-    MovieList,
-    PageNav,
   },
   computed: {
     isLogin() {
       return this.$store.getters.isLogin
     }
   },
-  created() {
-    this.getMovies()
-  },
   methods: {
-    getMovies() {
-      if (this.isLogin) {
-        this.$store.dispatch('get_movie_list', 1)
-      } else {
-        alert('로그인이 필요한 페이지입니다.')
-        this.$router.push({ name: 'login' })
-      }
-    }
+
   }
 }
 </script>
@@ -45,4 +41,9 @@ h1{
   font-family: 'Blinker', sans-serif;
   margin: 30px !important;
 }
+
+.genreBtn > button{
+  border:solid 2px black;
+}
+
 </style>
