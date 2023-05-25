d8cc9a8ccd27a4b27b938fb1efe1f78433b877e9
chadireoroonu <chadireoroonu@gmail.com>
2023-05-25 13:48:57 +0900

    SJ_DESIGN_home result

--- front-server/src/components/KarloImgsCarousel.vue
+++ front-server/src/components/KarloImgsCarousel.vue
@@ -1,20 +1,19 @@
 <template>
   <div>
-    <div v-if="karloImgs != null" id="finish">
-      <h1>이미지가 완성되었어요 !</h1>
-      <h3>영화 1: {{karloImgs[0].original_title1}}</h3>
-      <h3>영화 2: {{karloImgs[0].original_title2}}</h3>
-      <h3>선택한 화가: {{this.$store.state.selectedPainter}}</h3>
+    <div v-if="karloImgs != null" id="finish" style="margin: 6% 0% 3%">
+      <!-- <h1>이미지가 완성되었어요 !</h1> -->
+      <h3 style="margin-bottom: 1%;">{{ karloImgs[0].original_title1 }} & {{ karloImgs[0].original_title2 }}</h3>
+      <h3>by {{ this.$store.state.selectedPainter }}</h3>
     </div>
     <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src="http://127.0.0.1:8000/media/example.png" style="width: 30%; height: 30%;" alt="..1.">
+          <!-- <img src="http://127.0.0.1:8000/media/example.png" style="width: 30%; height: 30%;" alt="..1."> -->
         </div>
         <div class="carousel-item"
         v-for="karloImg in karloImgs"
         :key="karloImg.id">
-           <img :src="`http://127.0.0.1:8000${karloImg.img_url}`" style="width: 30%; height: 30%;" alt="">
+           <img :src="`http://127.0.0.1:8000${karloImg.img_url}`" style="width: 35%; height: 35;" alt="">
         </div>
       </div>
       <button v-if="karloImgs != null" class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
