e4687b8530c6fce0f92adf86551dd4f51df1118e
NayeonKim <kny0628m@gmail.com>
2023-05-26 04:16:38 +0900

    NY_FINAL

--- front-server/src/components/SearchModal.vue
+++ front-server/src/components/SearchModal.vue
@@ -4,7 +4,7 @@
     <div class="modal-dialog modal-lg modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalToggleLabel">영화 검색</h5>
+          <h5 class="modal-title" id="exampleModalToggleLabel">첫 번째 영화 선택하기</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
